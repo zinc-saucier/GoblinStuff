42787cd67cac61179a7ac71f0fe8fcd6de30d2a2
lateNightDebug <luna.mccormick@edu.sait.ca>
2026-04-14 15:13:50 -0600

    last minute minor polish

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Router, { useRouter } from "next/router";
 
-export default function NavBar() {
+interface HeaderProps {
+  currentPage: string;
+}
+export default function NavBar({ currentPage }: HeaderProps) {
+
   return (
     <div className="flex flex-row items-center m-2 p-2">
         <nav className="flex p-2 w-3/4 flex-row items-center justify-between flex-2">
-          <Link href="/">The Goblin's Stash</Link>
+    
+          <Link href="/" className={currentPage === 'home' ? 'text-orange-600' : 'text-gray-900'}>The Goblin's Stash</Link>
           {/* <Link href="/sack">The Sack</Link> */}
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart" className={currentPage === 'cart' ? 'text-orange-600' : 'text-gray-900'}>Cart</Link>
         </nav>
     </div>
   );
